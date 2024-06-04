3689d1c95d795057fa40d9c4c03ad13b63731778
Khizarali07 <khizarali.cs@gmail.com>
2024-06-04 15:31:01 +0500

    new

--- node.js
+++ node.js
@@ -47,7 +47,7 @@ app.use(cors());
     });
   });
 
-  app.get("/login", (req, res) => {
+  app.get("/", (req, res) => {
     app.use(express.static(path.resolve(__dirname, "front-end", "build")));
     res.sendFile(path.resolve(__dirname, "front-end", "build", "index.html"));
   });
