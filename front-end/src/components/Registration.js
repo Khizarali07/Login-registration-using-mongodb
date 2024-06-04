a2814202a40b0525f6c37cc79c7035d4f7fd9a30
Khizarali07 <khizarali.cs@gmail.com>
2024-06-04 15:12:32 +0500

    project added

---
+++ front-end/src/components/Registration.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import "./style.css";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+export default function Registration() {
+  const [name, setname] = useState("");
+  const [user, setuser] = useState("");
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+
+  const handlesubmit = (evt) => {
+    evt.preventDefault();
+    axios.post(`${window.location.origin}/registerdone`, {
+      name,
+      user,
+      email,
+      password,
+    });
+  };
+  return (
+    <>
+      <form onSubmit={handlesubmit}>
+        <fieldset id="login">
+          <input
+            type="text"
+            placeholder="Enter your name"
+            className="input"
+            value={name}
+            onChange={(evt) => {
+              setname(evt.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Enter your User-name"
+            className="input"
+            value={user}
+            onChange={(evt) => {
+              setuser(evt.target.value);
+            }}
+          />
+          <input
+            type="email"
+            placeholder="Enter your Email Address"
+            className="input"
+            value={email}
+            onChange={(evt) => {
+              setemail(evt.target.value);
+            }}
+          />
+          <input
+            type="password"
+            placeholder="Enter your password"
+            className="input"
+            value={password}
+            onChange={(evt) => {
+              setpassword(evt.target.value);
+            }}
+          />
+          <input type="submit" className="input" />
+          <Link className="input one" to="/login" id="link">
+            Login
+          </Link>
+        </fieldset>
+      </form>
+    </>
+  );
+}
---
+++ node.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const mongodb = require("mongodb");
+const cors = require("cors");
+const alert = require("alert-node");
+const url = "mongodb://localhost:27017/user";
+const app = express();
+const path = require("path");
+
+app.use(express.json());
+app.use(cors());
+
+(async function database() {
+  const con = await mongodb.MongoClient.connect(url);
+  const db = await con.db("user");
+  app.post("/registerdone", (req, res) => {
+    const { name, user, email, password } = req.body;
+    const data = { name, user, email, password };
+    const data1 = { email };
+    let check1 = 0;
+    const find = db.collection("data").find(data1);
+    find.toArray().then((results) => {
+      if (results.length === 0) {
+        const insertion = db.collection("data").insertOne(data);
+        alert("user registration done!!!");
+      } else {
+        alert("user already existing !!!");
+      }
+    });
+  });
+
+  app.post("/logindone", (req, res) => {
+    const { email, password } = req.body;
+    const data = { email, password };
+    const login = db.collection("data").find(data);
+
+    let x = login.forEach(
+      (doc = (result) => {
+        if (result) {
+          alert("successfull !!!");
+        }
+      })
+    );
+    login.toArray().then((results) => {
+      if (results.length === 0) {
+        alert("Login failed !!!");
+      }
+    });
+  });
+
+  app.get("/login", (req, res) => {
+    app.use(express.static(path.resolve(__dirname, "front-end", "build")));
+    res.sendFile(path.resolve(__dirname, "front-end", "build", "index.html"));
+  });
+
+  app.listen(3000);
+})();
