30929ba2d0121ee4ce93e011ea70e6aab6fce914
Khizarali07 <khizarali.cs@gmail.com>
2024-08-19 05:00:35 -0700

    Add files via upload

---
+++ front-end/src/App.js
@@ -0,0 +1,18 @@
+import Login from "./components/Login.js";
+import Register from "./components/Registration.js";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
